6dd21c625f9ecd59692f748d8bac47d1a56b7d31
Eriberto Guzman <eriberto176@gmail.com>
2020-05-29 20:30:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+  
+end
+
+
+def batch_badge_creator(badges)
+  badges.collect{|badge| badge_maker(badge)}
+  
+    
+end
+
+def assign_rooms(badges)
+
+  badges.collect.each_with_index{|badge, index| "Hello, #{badge}! You'll be assigned to room #{index+1}!"}
+end
+
+
+def printer(names)
+  names.each_with_index{|names, index| puts batch_badge_creator(names)[index] }
+end
